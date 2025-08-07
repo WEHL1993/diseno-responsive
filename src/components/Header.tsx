b64551ad8cc8a328a1e851071bc710f7ae71b3aa
Wilson Hernández López <wilsoneduardohl@gmail.com>
2025-08-06 21:58:44 -0600

    Se modifico el Header y se agrego un logotipo

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import logo from '../assets/programacion-web.png'; // ajusta el path si guardaste en otra carpeta
+
+function Header() {
+  return (
+    <header className="bg-dark text-white p-3">
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center gap-2">
+          <img src={logo} alt="Logo" style={{ height: '40px' }} />
+          <h1 className="h5 m-0">Desarrollo Web</h1>
+        </div>
+        <nav>
+          <ul className="nav">
+            <li className="nav-item"><a className="nav-link text-white" href="#">Inicio</a></li>
+            <li className="nav-item"><a className="nav-link text-white" href="#">Servicios</a></li>
+            <li className="nav-item"><a className="nav-link text-white" href="#">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
