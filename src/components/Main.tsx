d1e8a9ddc189639a984f9e924f2b19a9d9a3e83f
Wilson Hernández López <wilsoneduardohl@gmail.com>
2025-08-06 22:19:19 -0600

    Se modifico el Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <main className="py-5 text-center bg-light">
+      <div className="container">
+        <h2 className="display-5 fw-bold mb-3">Soluciones modernas en Desarrollo Web</h2>
+        <p className="lead text-muted">
+          Nuestra plataforma está diseñada para ayudarte a comprender, crear y mejorar sitios web de manera profesional.
+          Aquí encontrarás recursos útiles, ejemplos prácticos y servicios personalizados para impulsar tus habilidades
+          y proyectos en el mundo digital.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
